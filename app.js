7782e6c05d81be49faefefb4d5cfa55be6093b6e
virginiachae <chae.virginia@gmail.com>
2016-09-16 21:37:18 -0700

    finished basic game without oop

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
 $(".ravenclaw").click(function(){
 	console.log("sanity check: ravenclaw clicked!");
-	$(this).css('background', '#272892');
 	$(this).addClass( "ravenclawClicked" );
-	if (($("#ravenclawOne").hasClass("ravenclawClicked")) && ($("#ravenclawTwo").hasClass("ravenclawClicked"))){
-	console.log("yaaaaas! you found all the ravenclaws!");
 	checkWinner();
-	}
 });
 
 $(".gryffindor").click(function(){
 	console.log("sanity check: gryffindor clicked!");
-	$(this).css('background', '#db4536');
 	$(this).addClass("gryffindorClicked");
-	if (($("#gryffindorOne").hasClass("gryffindorClicked")) && ($("#gryffindorTwo").hasClass("gryffindorClicked"))){
-	console.log("yaaaaas! you found all the gryffindors");
 	checkWinner()
-	}
 });
 
 function checkWinner() {
 	var allCards = $(".card");
-	for (var i=0; i<allCards.lenth; i++);
+	for (var i=0; i<=allCards.length; i++);
 		if (($("#ravenclawOne").hasClass("ravenclawClicked")) 
 			&& ($("#ravenclawTwo").hasClass("ravenclawClicked")) 
 			&& ($("#gryffindorOne").hasClass("gryffindorClicked")) 
 			&& ($("#gryffindorTwo").hasClass("gryffindorClicked"))){
 			window.alert("GAME OVER!")
 		}
-		else ()
+		else if (($("#ravenclawOne").hasClass("ravenclawClicked")) 
+			&& ($("#ravenclawTwo").hasClass("ravenclawClicked"))){
+			console.log("yaaaaas! you found all the ravenclaws!");
+		}
+		else if (($("#gryffindorOne").hasClass("gryffindorClicked")) 
+			&& ($("#gryffindorTwo").hasClass("gryffindorClicked"))){
+			console.log("yaaaaas! you found all the gryffindors");
+		}
+		else if (($(".card").hasClass("ravenclawClicked")) 
+			&& ($(".card").hasClass("gryffindorClicked"))){
+				window.alert("Please try again :(");
+				clearCard();											
+		}
 }
+function clearCard(){
+	$(".card").removeClass ("ravenclawClicked gryffindorClicked")
+};
+			
+				
+	
+
+
+
 
 
