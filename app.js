9d99632dbe1ee41de76c94d0dca9d49896db609e
virginiachae <chae.virginia@gmail.com>
2016-09-16 17:35:40 -0700

    mvp stage

---
+++ app.js
@@ -0,0 +1,10 @@
+
+$(".ravenclaw").click(function(){
+	console.log("sanity check: ravenclaw clicked!");
+	$(this).css('background', '#272892')
+});
+
+$(".gryffindor").click(function(){
+	console.log("sanity check: gryffindor clicked!");
+	$(this).css('background', '#db4536')
+});
