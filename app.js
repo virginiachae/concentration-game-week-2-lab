25c8c291d6564fd4f9e6d8094d962abe4d96d56f
virginiachae <chae.virginia@gmail.com>
2016-09-16 19:34:53 -0700

    added winning parameters

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-
 $(".ravenclaw").click(function(){
 	console.log("sanity check: ravenclaw clicked!");
-	$(this).css('background', '#272892')
+	$(this).css('background', '#272892');
+	$(this).addClass( "ravenclawClicked" );
+	if (($("#ravenclawOne").hasClass("ravenclawClicked")) && ($("#ravenclawTwo").hasClass("ravenclawClicked"))){
+	console.log("yaaaaas! you found all the ravenclaws!");
+	checkWinner();
+	}
 });
 
 $(".gryffindor").click(function(){
 	console.log("sanity check: gryffindor clicked!");
-	$(this).css('background', '#db4536')
+	$(this).css('background', '#db4536');
+	$(this).addClass("gryffindorClicked");
+	if (($("#gryffindorOne").hasClass("gryffindorClicked")) && ($("#gryffindorTwo").hasClass("gryffindorClicked"))){
+	console.log("yaaaaas! you found all the gryffindors");
+	checkWinner()
+	}
 });
+
+function checkWinner() {
+	var allCards = $(".card");
+	for (var i=0; i<allCards.lenth; i++);
+		if (($("#ravenclawOne").hasClass("ravenclawClicked")) 
+			&& ($("#ravenclawTwo").hasClass("ravenclawClicked")) 
+			&& ($("#gryffindorOne").hasClass("gryffindorClicked")) 
+			&& ($("#gryffindorTwo").hasClass("gryffindorClicked"))){
+			window.alert("GAME OVER!")
+		}
+		else ()
+}
+
+
